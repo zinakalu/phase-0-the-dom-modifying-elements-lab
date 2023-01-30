d8cba465f32281e51966d974e5e84bbf7d5adf29
zinakalu <zina.kalu@gmail.com>
2023-01-30 18:13:28 +0100

    created dom modifying elements lab

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Write your code here!
+const newHeader = document.createElement('h1');
+newHeader.id = 'victory';
+// const newHeader = document.getElementById('victory');
+newHeader.innerHTML = "ZINA is the champion";
